9221bf198b68ea122d654184af5091a3b318ef71
kaspernj <k@spernj.org>
2014-11-06 10:10:04 +0100

    A lot of cleanup + Shippable.

--- app/controllers/chatty/chats_controller.rb
+++ app/controllers/chatty/chats_controller.rb
@@ -2,27 +2,27 @@ require_dependency "chatty/application_controller"
 
 class Chatty::ChatsController < Chatty::ApplicationController
   before_action :set_chat, only: [:show, :edit, :update, :destroy, :messages, :handle, :close]
-  
+
   def index
     @ransack_params = params[:q] || {}
     @ransack = Chatty::Chat.ransack(@ransack_params.clone)
     @chats = @ransack.result.order(:id).reverse_order
   end
-  
+
   def show
     respond_to do |format|
-      format.json { render(:json => {:chat => @chat.json}) }
+      format.json { render(json: {chat: @chat.json}) }
       format.html { render :show }
     end
   end
-  
+
   def new
     @chat = Chatty::Chat.new
   end
-  
+
   def edit
   end
-  
+
   def create
     @chat = Chatty::Chat.new(chat_params)
 
@@ -32,7 +32,7 @@ class Chatty::ChatsController < Chatty::ApplicationController
       render action: 'new'
     end
   end
-  
+
   def update
     if @chat.update(chat_params)
       redirect_to @chat, notice: 'Chat was successfully updated.'
@@ -40,36 +40,36 @@ class Chatty::ChatsController < Chatty::ApplicationController
       render action: 'edit'
     end
   end
-  
+
   def destroy
     @chat.destroy
     redirect_to chats_url, notice: 'Chat was successfully destroyed.'
   end
-  
+
   def messages
     @messages = @chat.messages
-    render :partial => "messages", :layout => false
+    render partial: "messages", layout: false
   end
-  
+
   def handle
     @chat.handle!
-    @chat.create_activity :key => "chatty/chat.handled", :owner => current_user
+    @chat.create_activity key: "chatty/chat.handled", owner: current_user
     redirect_to chat_path(@chat)
   end
-  
+
   def close
     @chat.close
-    @chat.create_activity :key => "chatty/chat.closed", :owner => current_user
+    @chat.create_activity key: "chatty/chat.closed", owner: current_user
     redirect_to chat_path(@chat)
   end
-  
+
 private
-  
+
   def set_chat
     @chat = Chatty::Chat.find(params[:id])
     authorize! action_name.to_sym, @chat
   end
-  
+
   def chat_params
     params.require(:chat).permit(:user_type, :user_id, :resource_type, :resource_id)
   end
